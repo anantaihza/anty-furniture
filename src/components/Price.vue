5a43006d8178f4214c81ce09df9799fe75ac183f
Ananta Ihza Ramadhan <anantaihza21@gmail.com>
2020-08-14 13:39:14 +0700

    Add format price

---
+++ src/components/Price.vue
@@ -0,0 +1,17 @@
+<template>
+    <span>
+        {{ this.prefix + Number(this.value) }}
+    </span>        
+</template>
+
+<script>
+export default {
+    props: {
+        value: Number,
+        prefix: {
+            type: String,
+            default: 'Rp. '
+        }
+    }
+}
+</script>
