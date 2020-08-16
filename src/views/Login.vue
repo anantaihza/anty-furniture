04c7be1997e89f752dc31eee93ce78b57898fd47
Ananta Ihza Ramadhan <anantaihza21@gmail.com>
2020-08-16 21:19:54 +0700

    Edit login page and delete component formLogin

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,60 +1,108 @@
 <template>
   <div>
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12 col-lg-6 d-none d-lg-block">
-                <div class="row">
-                    <div class="strip"></div>
-                    <div class="brand">
-                        <h1>Anty</h1>
-                        <h1>Furniture</h1>
-                    </div>
-
-                </div>
+      <div class="row">
+        <div class="col-md-12 col-lg-6 d-none d-lg-block">
+          <div class="row">
+            <div class="strip"></div>
+            <div class="brand">
+              <h1>Anty</h1>
+              <h1>Furniture</h1>
             </div>
-            <div class="col-md-12 col-lg-6 my-auto">
-                <form-login></form-login>
-                
-            </div>
+          </div>
         </div>
+        <div class="col-md-12 col-lg-6 my-auto">
+          <div class="formulir">
+            <h1 class="d-block d-sm-block d-md-block d-lg-none">Anty Furniture</h1>
+            <form>
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" v-model="user.email" />
+                <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+              </div>
+              <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" v-model="user.password" />
+                <a href>Forgot Password ?</a>
+              </div>
+              <div class="d-flex justify-content-center mt-5">
+                <button class="btn" @click="login">LOGIN</button>
+              </div>
+            </form>
+            <router-link
+              class="d-flex justify-content-center mt-3"
+              to="/register"
+            >Don't have an account ?</router-link>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import FormLogin from "@/components/FormLogin.vue";
-
 
 export default {
   name: "login",
-  components: {
-    FormLogin
+  data() {
+    return {
+      user: {
+        email: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    login: function() {
+      let datanya = {
+        email: this.user.email,
+        password: this.user.password
+      };
+      console.log(datanya.email);
+      console.log(datanya.password);
+    }
   }
 };
 </script>
 
 <style scoped>
 .strip {
-    float: left;
-    overflow: auto;
-    width: 66px;
-    height: 55rem;
-    
-    background: #FD961E;
+  float: left;
+  overflow: auto;
+  width: 66px;
+  height: 55rem;
+
+  background: #fd961e;
 }
 .brand {
-    /* position: absolute; */
-    /* width: 600px;
-    height: 1024px;
-    left: 66px;
-    top: 0px; */
-    background: #70B2EE;
-    padding: 22.7rem 13rem;
-
+  background: #70b2ee;
+  padding: 22.7rem 13rem;
 }
 .brand h1 {
-    color: white;
-    font-family: 'Snippet', sans-serif;
-    font-size: 46px;
+  color: white;
+  font-family: "Snippet", sans-serif;
+  font-size: 46px;
+}
+.formulir {
+  padding: 14rem 8rem;
+}
+form .btn {
+  background-color: #fd961e;
+  padding-left: 2rem;
+  padding-right: 2rem;
+}
+form .btn:hover {
+  background-color: rgb(253, 138, 7);
+}
+.form-group label {
+  font-weight: 700;
+}
+.form-group input {
+  border-radius: 10px;
+}
+.form-group input:focus {
+  border: none;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
 }
 </style>
